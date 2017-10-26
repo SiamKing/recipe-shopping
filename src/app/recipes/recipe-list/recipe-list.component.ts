d7b5c130303b854bb04a848d3114212c5a0463d7
SiamKing <thesiameseelectric@gmail.com>
2017-10-26 13:22:08 -0700

    created model for recipe and set up html for list and detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../Recipe';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://veryveganrecipes.com/wp-content/uploads/2016/09/Spicy-Tofu-And-Vegan-Enchilada-Bowl-Recipe.png')
+  ];
 
   constructor() { }
 
